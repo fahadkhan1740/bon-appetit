8d60223cb71128f7bea9848cf568a045c86f74a1
Fahad Khan <fahadkhan1740@outlook.com>
2023-08-10 13:45:17 +0530

    Added method signatures

--- app/Models/Ingredients.php
+++ app/Models/Ingredients.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static paginate()
+ * @method static create(array $all)
+ */
 class Ingredients extends Model
 {
     use HasFactory;
